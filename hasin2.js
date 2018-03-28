0c1dbe430bbf2769d7e389856ec1df392b2630e2
rushdi hasan <rushdincb@gmail.com>
2018-03-28 16:13:06 +0200

    added javascript

---
+++ hasin2.js
@@ -0,0 +1,35 @@
+var bob = new Object()
+	bob.name = "Bob Smth";
+	bob.age = 20;
+	bob.setAge=function(newAge){
+    	bob.age = newAge;
+    };
+
+
+bob.setAge(34);
+
+bob.setAge
+//console.log(bob.age);
+
+//another one
+
+var car = new Object();  //bject created
+car.name = "Mazda";     //property name
+car.price = "134560 euro"; // property price
+
+//creating the function to change the price
+
+car.setPrice=function(newPrice){
+	car.price = newPrice;
+	}
+
+
+car.setPrice("10000 euro"); // calling the  function with the changed price
+
+console.log(car.price); //now  log to see if the  price is changed  or not 
+
+
+ 
+
+
+
