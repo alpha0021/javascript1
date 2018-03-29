370e78c0511bb3c2393f33e9619bbb6594ac2def
rushdi hasan <rushdincb@gmail.com>
2018-03-30 00:20:26 +0200

    how to create  array of objects

--- hasin2.js
+++ hasin2.js
@@ -145,20 +145,65 @@ var ruba = new Person("nuba ",62);
 
 //constructor with this
 
-function Person(name,age){
-	this.name = name;
+function Person(firstName,lastName,age){
+	this.firstName =firstName;
+	this.lastName= lastName;
 	this.age=age;
 	this.sepiens = "Homo Sepiens";
+	this.fullName = function(){
+		return this.firstName + this.lastName;
+	}
 }
 
 
-var person1 = new Person("Rushdi hasan" ,29);
+var person1 = new Person("Rushdi" ,"hasan" ,29);
 var person2 = new Person("Subit Das",33);
 
 
-console.log("this is " + person1.name + " " + "and"+ " " + person1.name + "  "  + "is" 
-	+person1.age+ "years old");
-console.log("The  sepiens of this man is " + person1.sepiens);
+//console.log("this is " + person1.name + " " + "and"+ " " + person1.name + "  "  + "is" 
+//	+person1.age+ "years old");
+//console.log("The  sepiens of this man is " + person1.sepiens);
+
+//constructor with methods
+
+//console.log("The full name of person1 is " + person1.fullName());
+
+function  Person(adjective){
+	this.adjective = adjective;
+	this.describeMySelf = function(){
+		return "this man "+ " is " + this.adjective ;
+	}
+}
+
+var javed = new Person("angry");
+
+//console.log("javed is very " + javed.adjective);
+//console.log("javed is very " + javed.describeMySelf());
+
+
+//arrays of  objects
+
+function Person(name,age){
+	this.name = name;
+	this.age=age;
+}
+
+
+var family = new Array();   // new array family  created
+
+family[0] = new Person("rushdi hasan",22);
+
+
+console.log(family[0]);
+
+
+
+
+
+
+
+
+
 
 
 
