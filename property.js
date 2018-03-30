849329eceedf6bf76bcf88dd5cb4d5ae49eb4bf3
rushdi hasan <rushdincb@gmail.com>
2018-03-30 23:59:16 +0200

    how to access properties by dor  and  bracket notation

---
+++ property.js
@@ -0,0 +1,12 @@
+var bmw = new Object();
+
+bmw.price = 123000;
+bmw.model = "dfv23444";
+
+
+//accesing  proeprties using  Dot notation and Bracket notation
+var mazda = bmw.price;
+var ferrari = bmw["model"];
+
+console.log(mazda);
+console.log(ferrari)
