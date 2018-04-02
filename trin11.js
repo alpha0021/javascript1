28e1447fce35fb9293c16802ed784af59bfd358c
rushdi hasan <rushdincb@gmail.com>
2018-04-03 00:01:41 +0200

    passing  params to  method and its  use

---
+++ trin11.js
@@ -0,0 +1,21 @@
+var car = {
+	model:"eee34e444",
+	price:23456,
+	engine:function(metal){
+		console.log("the engine is prepared by" + metal);
+	}
+}
+
+
+car.engine("korpur");
+
+var moni = {
+	model:"2222dcvfff",
+	price:12345,
+
+	body:function(metal){
+		console.log("this body is made by" + metal);
+	}
+}
+
+moni.body("hard rock");
