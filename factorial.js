6a6dd9e15bed5cf9bbf2eece99b13acb44af071a
rushdi hasan <rushdincb@gmail.com>
2018-04-03 22:30:58 +0200

    finding the factorial using for loop

---
+++ factorial.js
@@ -0,0 +1,46 @@
+//finding the  factorial  using  for loop
+//instructor  hasin hayder
+//practise code rushdi hasan
+
+
+var n = 6;
+
+var factorial = 1;
+
+
+for(var i = n ; i > 1 ; i--){
+	factorial *= i;
+}
+
+//console.log(factorial);
+
+
+
+//more fatorial  practises
+
+
+
+
+var n = 3;
+
+var factorial = 1;
+
+for(var i = n ; i > 1 ;i--){
+	factorial*= i;
+}
+
+//console.log(factorial);
+
+
+//another 1 
+
+var n = 8;
+
+var factorial = 1;
+
+for(var i = n ; i > 1; i--){
+	factorial *= i ;
+}
+
+console.log(factorial);
+
