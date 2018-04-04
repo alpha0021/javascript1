dd93f8374fb0313e1ccd8106ccaa7e0652eadf30
rushdi hasan <rushdincb@gmail.com>
2018-04-04 22:04:41 +0200

    printing the properties of the object using a for loop

---
+++ printProperty.js
@@ -0,0 +1,25 @@
+var person = {
+	name:"nasrul kadir sunny",
+	age:23,
+	phone:3543223456,
+	email:'nasrulncb@gmail.com'
+}
+
+
+
+for(var property in person){
+	console.log(property);
+}
+
+
+
+var phoneBook = {
+	person:'rushdi hasan',
+	address:'360/a via nizza, torino',
+	service:'vodaphone'
+}
+
+
+for(var property in phoneBook){
+	console.log(property);
+}
