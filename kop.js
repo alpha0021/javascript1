635a9155072a5d93a429f92b75d6a4d023bbf3c7
rushdi hasan <rushdincb@gmail.com>
2018-04-04 21:36:23 +0200

    how to know the declared property exists in the declared object

---
+++ kop.js
@@ -0,0 +1,22 @@
+//how to know the declared property exists in the declared object
+
+var rushdi = {
+	name:'rushdi hasan',
+	age:22,
+	phone:3512833832
+}
+
+//console.log(rushdi.hasOwnProperty('age'));
+
+
+
+
+var idm = {
+	model:"12wsd34ffui",
+	price:123490,
+	maker:"chaina"
+}
+
+
+console.log(idm.hasOwnProperty('maker'));
+
