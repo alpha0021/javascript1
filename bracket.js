c51ea55af57886007683df345aecac6baf9be463
rushdi hasan <rushdincb@gmail.com>
2018-04-04 19:48:07 +0200

    use of bracket notation

---
+++ bracket.js
@@ -0,0 +1,34 @@
+var james = {
+    job: "programmer",
+    married: false
+};
+
+// set to the first property name of "james"
+var aProperty = "job";
+
+// print the value of the first property of "james" 
+// using the variable "aProperty"
+//console.log(james[aProperty]);
+
+
+var sunny = {
+  job:"student",
+  married:true
+}
+
+
+var aProperty = 'job';
+
+//console.log(james[aProperty]);
+
+var ball = {
+  color:'blue',
+  size:123
+}
+
+var aProperty = 'size';
+
+console.log(ball[aProperty]);
+
+
+
